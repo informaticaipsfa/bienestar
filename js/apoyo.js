ab88540e3a784fbeb54a5a846a2fb53c35643bed
root <root@localhost.localdomain>
2017-09-28 18:34:00 -0400

    cambios

--- js/apoyo.js
+++ js/apoyo.js
@@ -400,6 +400,7 @@ function habilitarDireccion(estatus) {
     } else {
         $("#btnhabdire").show();
         $("#btndhabdire").hide();
+        $("#btnhabdire").prop('disabled', false);
     }
 }
 
