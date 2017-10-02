f4112cd01ad0aa5d14873e1ca13bab57ed1521dd
Carlos E. Peña <gesaodin@gmail.com>
2017-10-02 18:11:34 -0400

    #Requisitos, agregar el texto

--- js/requerimientos_invsocial.js
+++ js/requerimientos_invsocial.js
@@ -1,4 +1,4 @@
 var investigacionsocial =[
-"Copia de la Cédula de Identidad y del Carnet Vigente del Militar y del Familiar ampliada y por ambas caras",
+"Copia de la Cédula de Identidad y del Carnet vigente del militar y del familiar ampliada por ambas caras (sí aplica)",
 "Informe Médico Firmado y Sellado por el Médico Tratante"
 ];
