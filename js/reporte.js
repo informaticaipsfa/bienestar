5fc1b60b297c61ced5bc73c7b393883c12988bb2
Carlos E. Peña <gesaodin@gmail.com>
2017-12-05 16:21:12 -0400

    Control de finanzas

--- js/reporte.js
+++ js/reporte.js
@@ -49,9 +49,6 @@ function ConsultarReembolsoReporte(){
   t.clear().draw();
   promesa.then(function(xhRequest) {
       respuesta = JSON.parse(xhRequest.responseText);
-      // console.log(respuesta);
-
-      //
       var i = 0;
       var mnt = 0;
       respuesta.forEach( v => {
@@ -85,19 +82,63 @@ function GenerarOficio(){
   var t = $('#lstReporte').DataTable();
   var num = t.rows().data().length;
   var mont = 0;
-
   for(i = 0; i <= t.rows().data().length - 1; i++) {
-    t.rows( i )
-      .data()
-      .each( function ( value, index ) {
-          // var valor = value.split(",");
-          //console.log( value[7] );
-
+    t.rows( i ).data().each( function ( value, index ) {
           mont += parseFloat(value[7])
       } );
   }
-  var ventana = window.open(`rpt/reembolso/oficiofinanza.html?num=${num}&mont=${mont}`, "_blank");
+  var oficio = `rpt/reembolso/oficiofinanzare.html?num=${num}&mont=${mont}`;
+  if ($("#cmbreportewrr").val() == "apoyo")
+    oficio = `rpt/reembolso/oficiofinanza.html?num=${num}&mont=${mont}`;
+
+  var ventana = window.open(oficio, "_blank");
 }
 function GenerarReporte(){
+  var t = $('#lstReporte').DataTable();
+  var tabla = $('#lstReporte').html();
+  var num = t.rows().data().length;
+  var mont = 0;
+  var desde = $("#txtfechadesdewrr").val();
+  var hasta = $("#txtfechahastawrr").val();
+
+  var meses = new Array ("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+  var f = new Date();
+  var fecha = "Caracas, " + f.getDate() + " de " + meses[f.getMonth()] + " de " + f.getFullYear();
+
+  for(i = 0; i <= t.rows().data().length - 1; i++) {
+    t.rows( i ).data().each( function ( value, index ) {
+          mont += parseFloat(value[7])
+      } );
+  };
+  var cabecera = `
+    <table width="100%">
+        <tr>
+            <td width="106" height="102"><img src="images/lg.png" width="106" height="100"></td>
+            <td width="288" style="font-size: 10px;">REPÚBLICA BOLIVARIANA DE VENEZUELA <br>
+                MINISTERIO DEL PODER POPULAR PARA LA DEFENSA<br>
+                VICEMINISTERIO DE SERVICIOS<br>
+                DIRECCIÓN GENERAL DE EMPRESAS Y SERVICIOS<br>
+                INSTITUTO DE PREVISIÓN SOCIAL DE LA FUERZA ARMADA<br>
+                GERECIA DE BIENESTAR Y SEGURIDAD
+          </td>
+            <td width="350" >
+          </td>
+            <td width="236">
+          </td>
+        </tr>
+    </table>
+    <br><br><p align="right">` + fecha + `</p><br>RELACION DE CASOS APROBADOS<br>
+    POR LA GERENCIA DE BIENESTAR SOCIAL NUMERO: __________<br><br>`;
+  var pie = `<br><br><br>
+  <b>EDUARDO JOSÉ MARTÍNEZ SALAS</b><br><br>
+
+  <center style="font-size: 10px;">“CHAVEZ VIVE LA PATRIA SIGUE”<br>
+  “INDEPENDENCIA Y PATRIA SOCIALISTA... “¡VIVIREMOS Y VENCEREMOS!</center>`;
+
+  var html = `<center><div style="width: 1000px;">` + cabecera + `<table width="100%" border=1>` + tabla + `</table>` + pie + `</div></center>`;
+  var ventana = window.open("", "_blank");
+  ventana.document.write(html);
+  ventana.document.head.innerHTML = ``;
+
 
 }
